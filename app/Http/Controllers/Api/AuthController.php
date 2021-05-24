3f79ce3b44547d811dbb7eac71c9bd8b7913c342
Md. Sakir Ahmed <52326976+mdsakirahmed@users.noreply.github.com>
2021-05-25 00:13:16 +0600

    api controller

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function login(Request $request){
+        $request->validate([
+            'email' => 'required',
+            'password' => 'required',
+        ]);
+
+        $user = User::where('email', $request->email)->first();
+
+        if(!$user || !Hash::check($request->password, $user->password)){
+            return response()->json([
+                'message' => 'Invalid user or password',
+            ]);
+        }
+
+        return $user->createToken('AuthToken')->accessToken;
+    }
+}
